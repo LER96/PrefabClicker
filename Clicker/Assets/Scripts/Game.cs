5376fabfa6063df36e735dba1a1b839360760fd4
LER96 <93140643+LER96@users.noreply.github.com>
2022-04-05 18:31:37 +0300

    Increasing Leaves

--- Clicker/Assets/Scripts/Game.cs
+++ Clicker/Assets/Scripts/Game.cs
@@ -2,37 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using System;
 
 public class Game : MonoBehaviour
 {
     public Text ui;
-public void Increment()
+    public List<int> shovel;
+    public List<int> tree;
+    public List<int> airgun;
+    public void Increment()
     {
-        GameManager.leave += GameManager.multiplier;
+        //GameManager.leave += GameManager.multiplier;
+        GameManager.leave += 1;
     }
 
     public void Buy(int num)
     {
         if (num == 1 && GameManager.leave >= 25)
         {
-            GameManager.multiplier += 1;
+            //GameManager.multiplier += 1;
+            shovel.Add(1);
             GameManager.leave -= 25;
         }
         if (num == 2 && GameManager.leave >= 40)
         {
-            GameManager.multiplier += 10;
+            //GameManager.multiplier += 10;
+            tree.Add(2);
             GameManager.leave -= 40;
         }
         if (num == 3 && GameManager.leave >= 80)
         {
-            GameManager.multiplier += 100;
+            //GameManager.multiplier += 100;
+            tree.Add(3);
             GameManager.leave -= 80;
         }
     }
 
     public void Update()
     {
-        ui.text = "Leaves: " + GameManager.leave;
+        //
+        GameManager.leave += shovel.Count* Time.fixedDeltaTime*0.1f;
+        GameManager.leave += tree.Count * Time.fixedDeltaTime*0.5f;
+        GameManager.leave += airgun.Count * Time.fixedDeltaTime;
+
+        ui.text = "Leaves: " + (int)GameManager.leave;
     }
+
+
 }
 
--- Clicker/Assets/Scripts/GameManager.cs
+++ Clicker/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static int leave;
+    public static float leave;
     public static int multiplier;
     void Start()
     {
